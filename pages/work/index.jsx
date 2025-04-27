73571c0e84cb53a5c2471ac72541abbb3634a63a
BayuWinataa <bayuwinata94@gmail.com>
2025-04-27 15:50:50 +0700

    edit bug project

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -10,10 +10,10 @@ const Work = () => {
 		<div className="h-full bg-primary/30 py-36 flex items-center">
 			<Circles />
 			<div className="container mx-auto">
-				<div className="flex flex-col xl:flex-row gap-x-8">
+				<div className="flex flex-row gap-x-4 xl:gap-x-8">
 					{/* text */}
-					<div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-						<motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className="h2 xl:mt-12">
+					<div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0 ">
+						<motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className="h2 mt-36 xl:mt-12 ">
 							Karya Saya <span className="text-accent">.</span>
 						</motion.h2>
 						<motion.p variants={fadeIn('up', 0.4)} initial="hidden" animate="show" exit="hidden" className="mb-4 max-w-[400px] mx-auto lg:mx-0">
@@ -23,7 +23,7 @@ const Work = () => {
 					</div>
 
 					{/* slider */}
-					<motion.div variants={fadeIn('down', 0.6)} initial="hidden" animate="show" exit="hidden" className="w-full xl:max-w-[65%] cursor-grab">
+					<motion.div variants={fadeIn('down', 0.6)} initial="hidden" animate="show" exit="hidden" className="w-full  pt-40 xl:pt-0 max-w-[55%] xl:max-w-[65%] cursor-grab">
 						<WorkSlider />
 					</motion.div>
 				</div>
