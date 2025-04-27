c825d64599bdeb483c255fba6f2930eb027f2208
BayuWinataa <bayuwinata94@gmail.com>
2025-04-27 14:48:11 +0700

    edit bug home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 
-
 import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
 import Avatar from '../components/Avatar';
@@ -14,13 +13,13 @@ const Home = () => {
 			<div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
 				<div className="text-center flex flex-col justify-center xl:pt-32 xl:text-left h-full container mx-auto">
 					{/* title */}
-					<motion.h1 variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" className="h1">
+					<motion.h1 variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" className="h1 text-4xl xl:text-7xl">
 						Frontend Web <br />
 						<span className="text-accent">Developer</span>
 					</motion.h1>
 
 					{/* subtitle */}
-					<motion.p variants={fadeIn('down', 0.3)} initial="hidden" animate="show" exit="hidden" className="max-w-sm xl:max-w-xl mx-0 mb-10  ">
+					<motion.p variants={fadeIn('down', 0.3)} initial="hidden" animate="show" exit="hidden" className="max-w-full xl:max-w-xl mx-0 mb-5  ">
 						Saya seorang Frontend Developer yang fokus pada desain dan pengembangan tampilan web yang responsif, modern, dan mudah digunakan. Menggabungkan estetika visual dengan fungsionalitas adalah prioritas saya dalam setiap proyek.
 					</motion.p>
 
@@ -29,7 +28,6 @@ const Home = () => {
 						<ProjectsBtn />
 					</div>
 
-          
 					<motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show" exit="hidden" className="hidden xl:flex">
 						<ProjectsBtn />
 					</motion.div>
