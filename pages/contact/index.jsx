afabce9e90472ddec7b0afc110d9454c7690f1b7
BayuWinataa <bayuwinata94@gmail.com>
2025-04-27 16:26:38 +0700

    edit bug contact

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
 				{/* text & form */}
 				<div className="flex flex-col w-full max-w-[700px]">
 					{/* text */}
-					<motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className="h2 text-center mb-12">
+					<motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className="h2 text-center  mb-2 xl:mb-12 text-4xl xl:text-6xl">
 						Let's <span className="text-accent">connect.</span>
 					</motion.h2>
 
@@ -41,7 +41,7 @@ const Contact = () => {
 						initial="hidden"
 						animate="show"
 						exit="hidden"
-						className="flex-1 flex flex-col gap-6 w-full mx-auto"
+						className="flex-1 flex flex-col gap-4 w-full mx-auto"
 						onSubmit={handleSubmit}
 						autoComplete="off"
 						autoCapitalize="off"
@@ -50,11 +50,11 @@ const Contact = () => {
 					>
 						{/* input group */}
 						<div className="flex gap-x-6 w-full">
-							<input type="text" name="name" placeholder="Name" className="input" disabled={isLoading} aria-disabled={isLoading} required aria-required />
+							<input type="text" name="name" placeholder="Name" className="input " disabled={isLoading} aria-disabled={isLoading} required aria-required />
 							<input type="email" name="email" placeholder="E-mail" className="input" disabled={isLoading} aria-disabled={isLoading} required aria-required />
 						</div>
 						<input type="text" name="subject" placeholder="Subject" className="input" disabled={isLoading} aria-disabled={isLoading} required aria-required />
-						<textarea name="message" placeholder="Message..." className="textarea" disabled={isLoading} aria-disabled={isLoading} required aria-required />
+						<textarea name="message" placeholder="Message..." className="textarea h-20" disabled={isLoading} aria-disabled={isLoading} required aria-required />
 						<button
 							type="submit"
 							className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
